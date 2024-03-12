747c40e097d1008c84babddab042588faa1ecdd5
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-12 15:28:04 +0500

    real

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello from index.js");
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDxwbTPPXK_TJ58-CUtda_xij6e81SGSpw",
+  authDomain: "wired-rex-414706.firebaseapp.com",
+  projectId: "wired-rex-414706",
+  storageBucket: "wired-rex-414706.appspot.com",
+  messagingSenderId: "326739103673",
+  appId: "1:326739103673:web:46888e1009097132ebb068",
+  measurementId: "G-5LF60E44R7",
+};
+
+initializeApp(firebaseConfig);
